ee2223ab9068b6b3b3c75c2a23e68c1e59215b14
Prasad Sawant <prasad.sawant20397@gmail.com>
2024-09-02 00:33:13 +0530

    final fix

--- idx-template.nix
+++ idx-template.nix
@@ -1,11 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+# No user-configurable parameters
+{ pkgs, ... }: {
+  # Shell script that produces the final environment
+  bootstrap = ''
+    # Copy the folder containing the `idx-template` files to the final
+    # project folder for the new workspace. ${./.} inserts the directory
+    # of the checked-out Git folder containing this template.
+    cp -rf ${./.} "$out"
 
-pkgs.stdenv.mkDerivation {
-  name = "html-css-idx-template";
-  src = ./.;
-  buildInputs = [ pkgs.coreutils ];
-  installPhase = ''
-    mkdir -p $out
-    cp -r index.html styles.css idx-template.json .idx/dev.nix $out/
+    # Set some permissions
+    chmod -R +w "$out"
+
+    # Remove the template files themselves and any connection to the template's
+    # Git repository
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
   '';
 }
